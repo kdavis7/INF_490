74e66fd2bc0d0a4605e20f327c011a2b3e9325b2
Kaitlyn <kndavis@mail.fhsu.edu>
2023-03-08 17:07:12 -0500

    read_single categories

---
+++ api/categories/read_single.php
@@ -0,0 +1,25 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type:application/json');
+    
+    require('../../config/Database.php');
+    require('../../models/Category.php');
+
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $categories = new Category($db);
+
+    $categories->id = isset($_GET['id']) ? $_GET['id']: die();
+
+    
+
+    if($categories->read_single()) {
+      echo json_encode(array('id' => $categories->id,'category' => $categories->category));
+    }
+  else {
+      echo json_encode(array('message' => 'category_id Not Found'));
+
+  }
+  ?>
