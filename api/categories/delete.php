b81d5d6442cccb423f6e1f589de5b604b21d3b9c
Kaitlyn <kndavis@mail.fhsu.edu>
2023-03-08 16:54:23 -0500

    delete.php

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -1,7 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type:application/json');
-    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Methods: DELETE');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods,Content-Type, Authorization, X-Requested-With');
     
     require ('../../config/Database.php');
@@ -14,16 +14,15 @@
     $categories = new Category($db);
 
     $data = json_decode(file_get_contents("php://input"));
-    
-   
 
-    if(!isset($data->category)){
-        echo json_encode(array('message' => 'Missing Required Parameters'));
+    if (!isset($data->id)){
+        echo(json_encode(array('message' => 'Missing Required Parameters')));
     }
-    else {
-        $categories->category = $data->category;
-        $categories->create();
-        echo json_encode(array('id'=> $db->lastInsertId(),'category'=>$categories->category));
+    else
+    {
+        $categories->id = $data->id;
+        $categories->delete();
+        echo(json_encode(array('id'=>$categories->id)));
     }
 
 ?>
